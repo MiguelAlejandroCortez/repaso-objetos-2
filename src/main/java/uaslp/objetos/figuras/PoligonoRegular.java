eef481dedb80281364f81e478a59338f8239629f
Miguel Cortez <mickeycortez2002@gmail.com>
2022-12-06 22:50:18 -0600

    ejercicio 1 - 30 min

---
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -0,0 +1,32 @@
+package uaslp.objetos.figuras;
+
+public class PoligonoRegular {
+    private double lado;
+    private int numeroDeLados;
+
+    public PoligonoRegular(int numeroDeLados){
+
+    }
+    public PoligonoRegular(int numeroDeLados, double lado) {
+        this.lado = lado;
+        this.numeroDeLados = numeroDeLados;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea(){
+     double teta;
+     double apotema;
+
+     teta= (360/(numeroDeLados * numeroDeLados));
+     apotema = lado/(2* Math.tan(teta));
+
+     return ((lado*numeroDeLados) * apotema)/2;
+    }
+}
